c4074a0e37770351f8812605c7d486526aa527bf
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-27 14:58:11 +0100

    working on runner file - save & update working, but show all and delete are not

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,15 +1,36 @@
 import db.DBHelper;
-import models.Brand;
-import models.ProductType;
-import models.Stock;
+import models.*;
+
+import java.util.List;
 
 public class Runner {
 
     public static void main(String[] args) {
+
         Stock stock1 = new Stock(200, 5.99, Brand.Green_People, ProductType.Eyeliner);
         DBHelper.save(stock1);
         Stock stock2 = new Stock(300, 35.99, Brand.Eden, ProductType.Perfume);
         DBHelper.save(stock2);
+        Stock stock3 = new Stock(200, 10.99, Brand.Eden, ProductType.Mascara);
+        DBHelper.save(stock3);
+
+        stock2.setPrice(37.99);
+        DBHelper.update(stock2);
+
+        DBHelper.delete(stock3);
+
+
+        Customer customer1 = new Customer("Marie");
+        DBHelper.save((customer1));
+        Basket basket1 = new Basket(customer1);
+        DBHelper.save(basket1);
+
+        Shop shop = new Shop("Ethics & Cosmetics");
+        DBHelper.save(shop);
+
+        List<Stock> allStock = DBHelper.getAll(Stock.class);
+
+
 
     }
 }
