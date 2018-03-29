60aecf52db6eb702828e8fb68e829607f92cee81
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-29 21:37:24 +0100

    formatting

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -21,20 +21,20 @@ public class Seeds {
         DBHelper.save(stock0);
         Stock stock1 = new Stock(100, 5.99, "/images/dr-organic-logo.jpg",Brand.Dr_Organic, ProductType.Eyeliner);
         DBHelper.save(stock1);
-        Stock stock2 = new Stock(70, 35.99, Brand.Eden, ProductType.Perfume);
+        Stock stock2 = new Stock(70, 35.99,"/images/Arbonne.png", Brand.Arbonne, ProductType.Lipstick);
         DBHelper.save(stock2);
-//        Stock stock3 = new Stock(200, 10.99, Brand.Plant_Face, ProductType.Mascara);
-//        DBHelper.save(stock3);
-//        Stock stock4 = new Stock(400, 9.99, Brand.Arbonne, ProductType.Lipstick);
+        Stock stock3 = new Stock(200, 10.99, "/images/Eden.jpg", Brand.Eden, ProductType.Perfume);
+        DBHelper.save(stock3);
+//        Stock stock4 = new Stock(400, 9.99,"/images/Arbonne.png", Brand.Arbonne, ProductType.Lipstick);
 //        DBHelper.save(stock4);
-//        Stock stock5 = new Stock(320, 30.00, Brand.Pacifica, ProductType.Foundation);
-//        DBHelper.save(stock5);
-//        Stock stock6 = new Stock(110, 5.75, Brand.Sukin, ProductType.Concealer);
+        Stock stock5 = new Stock(320, 30.00, "/images/Pacifica.jpg", Brand.Pacifica, ProductType.Foundation);
+        DBHelper.save(stock5);
+//        Stock stock6 = new Stock(110, 5.75, "/images/sukin.png", Brand.Sukin, ProductType.Concealer);
 //        DBHelper.save(stock6);
-//        Stock stock7 = new Stock(0, 15.75, Brand.Dr_Organic, ProductType.Perfume);
+//        Stock stock7 = new Stock(0, 15.75, "/images/dr-organic-logo.jpg", Brand.Dr_Organic, ProductType.Perfume);
 //        DBHelper.save(stock7);
-//        Stock stock8 = new Stock(110, 25.75, Brand.Salt_of_the_Earth, ProductType.Eyeliner);
-//        DBHelper.save(stock8);
+        Stock stock8 = new Stock(110, 25.75, "/images/saltoftheearth-logo.jpg", Brand.Salt_of_the_Earth, ProductType.Eyeliner);
+        DBHelper.save(stock8);
     }
 }
 
